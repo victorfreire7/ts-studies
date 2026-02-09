462b5de184586f4c18f46bd996e9fcff5eb2d305
victorfreire7 <vhugo.freire159@gmail.com>
2026-02-09 19:59:17 -0300

    first commit

---
+++ src/001 class.ts
@@ -0,0 +1,46 @@
+/*
+  Atributos:
+    - public
+    - private
+    - protected
+    - readonly
+
+  - Semelhtante a Class no JS;
+  - funciona também como Type;
+  - Definição e depois Constructor;
+  - Podemos também definir dentro do constructor.
+*/
+
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  public addColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  public mostrarColaborador(): void {
+    for(var colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Victor', 'Hugo');
+empresa1.addColaborador(colaborador1);
+empresa1.mostrarColaborador();
+console.log(empresa1);
