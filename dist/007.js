641f5b30929a2d128beea15e57304ddbfd389352
victorfreire7 <vhugo.freire159@gmail.com>
2026-05-08 12:41:06 -0300

    clean branch

--- dist/007.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a, _b;
-const documento = {
-    titulo: 'O título',
-    texto: 'O texto',
-    data: new Date()
-};
-console.log((_b = (_a = documento.data) === null || _a === void 0 ? void 0 : _a.toDateString()) !== null && _b !== void 0 ? _b : 'Informação undefined');
